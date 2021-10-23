00d0d4b39f8271ccea24bb0546c599a330d7d035
Christian Clauss <cclauss@me.com>
2021-10-23 08:16:37 +0200

    Use print() function in both Python 2 and Python 3

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 # Copyright (c) 2016-2021 Twilio Inc.
 
 import os
@@ -80,7 +81,7 @@ def run_for(from_, inp):
             return str(resp)
 
         if not exists:
-            print 'starting new game for', from_
+            print('starting new game for', from_)
             handler = TwilioHandler()
             game = Game(handler)
             t = threading.Thread(target=game.go)
@@ -135,7 +136,7 @@ def run_for(from_, inp):
 
 @app.route("/incoming-voice", methods=['GET', 'POST'])
 def voice_reply():
-    print 'Form', request.form
+    print('Form', request.form)
     from_ = request.form['DialogueSid'][2:34]
     inp = ''
     if 'Field_word1_Value' in request.form:
